4061700b619f5f2988924fb1fa3a4634e5a520ff
Dan Ilin <dan.ilin@me.com>
2015-09-11 09:09:12 -0400

    Minor bug fixes.

--- src/music_scraper/core.clj
+++ src/music_scraper/core.clj
@@ -8,30 +8,32 @@
                :genre-tags   #"\[.*\]"
                :comment      #"[^)]*$"})
 
-(defn parse-track-data [track]
-  (let [artist-track (str/split (re-find (:artist-track patterns) track) #" --? ")]
-    {:artist     (str/trim (get artist-track 0))
-     :track      (str/trim (get artist-track 1))
-     :year       (subs (re-find (:year patterns) track) 1 5)
-     :genre-tags (let [tags (re-find (:genre-tags patterns) track)]
-                   (str/split (subs tags 1 (- (.length tags) 1)) #" "))
-     :comment    (re-find (:comment patterns) track)}))
+(def url "https://www.reddit.com/r/listentothis/new.json")
 
-(defn map-post [post]
+(defn parse-track-data [track]
   (try
-    (let [{data :data} post]
-      (let [parsed-data (parse-track-data (:title data))]
-        {:post-id       (:name data)
-         :time          (:created_utc data)
-         :media-url     (:url data)
-         :artist        parsed-data
-         :track         parsed-data
-         :parse-failed? (or (nil? (:artist parsed-data)) (nil? (:track parsed-data)))}))
+    (let [artist-track (str/split (re-find (:artist-track patterns) track) #" --? ")]
+      {:artist     (str/trim (get artist-track 0))
+       :track      (str/trim (get artist-track 1))
+       :year       (subs (re-find (:year patterns) track) 1 5)
+       :genre-tags (let [tags (re-find (:genre-tags patterns) track)]
+                     (str/split (subs tags 1 (- (.length tags) 1)) #" "))
+       :comment    (re-find (:comment patterns) track)})
     (catch Exception e
       (println e))))
+
+(defn map-post [post]
+  (let [{data :data} post]
+    (let [parsed-data (parse-track-data (:title data))]
+      {:post-id       (:name data)
+       :time          (:created_utc data)
+       :media-url     (:url data)
+       :artist        (:artist parsed-data)
+       :track         (:track parsed-data)
+       :parse-failed? (or (nil? (:artist parsed-data)) (nil? (:track parsed-data)))})))
 
 (defn get-page-data [body]
   (:data (json/read-str body :key-fn keyword)))
 
 (defn process-page [page]
-  (map #'map-post (:childriin page)))
+  (map #'map-post (:children page)))
