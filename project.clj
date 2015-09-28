b3818102bad5466b796ddaddbca11c4a5070bcca
Dan Ilin <dan.ilin@me.com>
2015-09-27 21:04:59 -0400

    Added spotify client namespace Added functions to authenticate and search tracks against spotify spi

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/data.json "0.2.6"]
                  [clj-http "2.0.0"]
-                 [org.clojure/data.json "0.2.6"]]
+                 [environ "1.0.1"]]
+  :plugins [[lein-environ "1.0.1"]]
   :main music-scraper.core)
---
+++ src/music_scraper/spotify/client.clj
@@ -0,0 +1,26 @@
+(ns music-scraper.spotify.client
+  (:require [environ.core :refer [env]]
+            [clj-http.client :as client]
+            [clojure.data.json :as json]))
+
+(def access-token (atom nil))
+
+(def spotify-creds
+  {:client-id     (env :spotify-client-id)
+   :client-secret (env :spotify-client-secret)})
+
+(defn authenticate [client-id client-secret]
+  (reset! access-token
+          (:access_token
+            (json/read-str (:body
+                             (client/post "https://accounts.spotify.com/api/token"
+                                          {:form-params {:grant_type "client_credentials"}
+                                           :accept      :json
+                                           :basic-auth  [(:client-id spotify-creds) (:client-secret spotify-creds)]}))
+                           :key-fn keyword))))
+
+; todo: use oauth token properly
+(defn search-spotify-track [track]
+  (client/get "https://api.spotify.com/v1/search"
+              {:query-params {:q track :type "track"}
+               :accept       :json}))
