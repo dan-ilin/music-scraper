5da9910e4b495730766a1bc682949079e116dd82
Dan Ilin <dan.ilin@me.com>
2015-10-29 21:59:05 -0400

    Changed spotify client to use component model

--- src/music_scraper/spotify/client.clj
+++ src/music_scraper/spotify/client.clj
@@ -3,50 +3,54 @@
             [clj-http.client :as client]
             [clojure.data.json :as json]
             [clojure.string :refer [join]]
-            [clojure.tools.logging :as log]))
+            [clojure.tools.logging :as log]
+            [com.stuartsierra.component :as component]))
 
-(def access-token (atom (env :access-token)))
+(defn refresh-token
+  ([client-id client-secret refresh-token]
+   (log/info "Refreshing Spotify access token")
+   (json/read-str (:body (client/post "https://accounts.spotify.com/api/token"
+                                      {:form-params {:grant_type    "refresh_token"
+                                                     :refresh_token refresh-token}
+                                       :accept      :json
+                                       :basic-auth  [client-id client-secret]}))
+                  :key-fn keyword))
+  ([client] (refresh-token (:client-id client) (:client-secret client) (:refresh-token client))))
 
-(def credentials {:client-id     (env :spotify-client-id)
-                  :client-secret (env :spotify-client-secret)
-                  :refresh-token (env :refresh-token)
-                  :user-id       (env :user-id)
-                  :playlist-id   (env :playlist-id)})
+(defrecord client [client-id client-secret refresh-token user-id playlist-id]
+  component/Lifecycle
 
-(defn refresh-token [client-id client-secret refresh-token]
-  (log/info "Refreshing Spotify access token")
-  (reset! access-token
-          (:access_token (json/read-str (:body (client/post "https://accounts.spotify.com/api/token"
-                                                            {:form-params {:grant_type    "refresh_token"
-                                                                           :refresh_token refresh-token}
-                                                             :accept      :json
-                                                             :basic-auth  [client-id client-secret]}))
-                                        :key-fn keyword))))
+  (start [component]
+    (let [resp (refresh-token client-id client-secret refresh-token)]
+      (assoc component :client-id client-id
+                       :client-secret client-secret
+                       :access-token (:access-token resp)
+                       :refresh-token (:refresh-token resp)
+                       :user-id user-id
+                       :playlist-id playlist-id)))
 
-(defn search-spotify-track [track]
+  (stop [component]
+    (assoc component :access-token nil)))
+
+(defn search-spotify-track [client track]
   (log/infof "Searching Spotify for %s" track)
   (Thread/sleep 25)
   (json/read-str (:body (client/get "https://api.spotify.com/v1/search"
                                     {:query-params {:q track :type "track"}
                                      :accept       :json
-                                     :oauth-token  @access-token}))
+                                     :oauth-token  (:access-token client)}))
                  :key-fn keyword))
 
-(defn add-to-playlist [tracks]
+(defn add-to-playlist [client tracks]
   (log/infof "Adding %d new tracks to Spotify playlist" (count tracks))
   (if (not (empty? tracks))
     (doseq [x (partition 10 tracks)]
       (Thread/sleep 100)
       (client/post (format "https://api.spotify.com/v1/users/%s/playlists/%s/tracks"
-                           (:user-id credentials)
-                           (:playlist-id credentials))
+                           (:user-id client)
+                           (:playlist-id client))
                    {:query-params {:uris (join "," x)}
-                    :oauth-token  @access-token}))))
+                    :oauth-token  (:access-token client)}))))
 
 (defn match-artist [artist result]
   (filter (fn [y] (.equalsIgnoreCase artist (:name y))) (:artists result)))
-
-(defn start []
-  (refresh-token (:client-id credentials)
-                 (:client-secret credentials)
-                 (:refresh-token credentials)))
