a3c24675afa8744913748d083f98f0268ab1ed11
Dan Ilin <dan.ilin@me.com>
2015-10-29 22:28:02 -0400

    Changed scraper to use component model

--- src/music_scraper/scraper.clj
+++ src/music_scraper/scraper.clj
@@ -4,34 +4,46 @@
             [music-scraper.spotify.client :as spotify]
             [music-scraper.database :as database]
             [clojure.tools.logging :as log]
-            [clj-http.client :as client]))
+            [clj-http.client :as client]
+            [com.stuartsierra.component :as component]))
 
-(def matched-tracks (atom []))
+(defn add-match [scraper match track]
+  (reset! (:matched-tracks scraper) (conj @(:matched-tracks scraper) (:uri match)))
+  (database/add-spotify-uri (:database scraper) track (:uri match)))
 
-(defn add-match [match track]
-  (reset! matched-tracks (conj @matched-tracks (:uri match)))
-  (database/add-spotify-uri track (:uri match)))
-
-(defn process-result [result]
+(defn process-result [scraper result]
   (if (not (:parse-failed? result))
-    (let [first-match (get (:items (:tracks (spotify/search-spotify-track (:track result)))) 0)]
-      (database/save-track result)
+    (let [first-match (get (:items (:tracks (spotify/search-spotify-track (:spotify scraper) (:track result)))) 0)]
+      (database/save-track (:database scraper) result)
       (if (not (empty? (spotify/match-artist (:artist result) first-match)))
-        (add-match first-match result)))))
+        (add-match scraper first-match result)))))
 
-(defn process-page [page]
-  (let [filtered-results (filter #(not (database/track-exists? (:id (:data %)))) (:children page))]
+(defn process-page [scraper page]
+  (let [filtered-results (filter #(not (database/track-exists? (:database scraper) (:id (:data %)))) (:children page))]
     (doseq [x (map #'parse/map-post filtered-results)]
-      (process-result x))))
+      (process-result scraper x))))
 
-(defn process [page]
-  (process-page page)
-  (Thread/sleep 500)
-  (if (not (nil? (:after page)))
-    (process (reddit/get-page reddit/url (:after page)))))
+(defn process
+  ([scraper]
+   (log/info "Processing results")
+   (process scraper (reddit/get-page-data
+                      (:body (client/get reddit/url {:accept        :json
+                                                     :client-params {"http.useragent" "music-scraper"}})))))
+  ([scraper page]
+   (process-page scraper page)
+   (Thread/sleep 500)
+   (if (not (nil? (:after page)))
+     (process scraper (reddit/get-page reddit/url (:after page))))))
 
-(defn start []
-  (log/info "Processing results")
-  (process (reddit/get-page-data (:body (client/get reddit/url {:accept        :json
-                                                                :client-params {"http.useragent" "music-scraper"}}))))
-  (spotify/add-to-playlist @matched-tracks))
+(defrecord Database [database spotify]
+  ;; Implement the Lifecycle protocol
+  component/Lifecycle
+
+  (start [scraper]
+    (assoc scraper :matched-tracks (atom [])
+                   :database database
+                   :spotify spotify))
+
+  (stop [scraper]
+    (spotify/add-to-playlist (:spotify scraper) (:matched-tracks scraper))
+    (assoc scraper :connection nil)))
