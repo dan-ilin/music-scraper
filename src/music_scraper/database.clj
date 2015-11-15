031176f795647c2e223b2d272e43560a00e35c52
dan <dan.ilin@me.com>
2015-11-15 15:33:57 -0500

    Refactored log-query function to accept db-spec arg directly

--- src/music_scraper/database.clj
+++ src/music_scraper/database.clj
@@ -6,31 +6,31 @@
 
 (defqueries "sql/tracks.sql")
 
-(defn log-query [db query args]
+(defn log-query [db-spec query args]
   (log/infof "Running Query %s with args: %s" query args)
   (try
-    (log/spyf "Query Results: %s" (apply query (:db-spec db) args))
+    (log/spyf "Query Results: %s" (apply query db-spec args))
     (catch Exception e (log/error e "Query failed for args:" args))))
 
 (defn save-track [db track]
-  (log-query db #'insert-track<! [(:post-id track)
-                                  (:time track)
-                                  (:media-url track)
-                                  (:artist track)
-                                  (:track track)]))
+  (log-query (:db-spec db) #'insert-track<! [(:post-id track)
+                                             (:time track)
+                                             (:media-url track)
+                                             (:artist track)
+                                             (:track track)]))
 
 (defn add-spotify-uri [db track uri]
-  (log-query db #'update-spotify-uri! [uri, (:post-id track)]))
+  (log-query (:db-spec db) #'update-spotify-uri! [uri, (:post-id track)]))
 
 (defn track-exists? [db post-id]
-  (:exists (log-query db #'track-exists [post-id])))
+  (:exists (log-query (:db-spec db) #'track-exists [post-id])))
 
 (defrecord Database [db-spec]
   component/Lifecycle
 
   (start [this]
     (log/info "Starting database")
-    (create-tracks! db-spec)
+    (log-query db-spec #'create-tracks! [])
     (assoc this :db-spec db-spec))
 
   (stop [this]
